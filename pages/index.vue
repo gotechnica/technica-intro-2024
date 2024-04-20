e91022e034eb638bc8a2987bd659c59c5cca8e29
Matthew Yu <matthew.yu904@gmail.com>
2024-04-20 17:28:22 -0400

    added instructions; own page

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,59 @@
 <template>
   <div class="flex-container">
     <nav id="navbar" class="navbar navbar-expand-lg navbar-dark">
-    <a class="navbar-brand" href="https://gotechnica.org">
-      <div class="logo"></div>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav text-center">
-        <li class="nav-item">
-          <a class="nav-link" href="./index" target="_blank">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./serena" target="_blank">Serena</a>
-        </li>
-      </ul>
+      <a class="navbar-brand" href="https://gotechnica.org">
+        <div class="logo"></div>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav text-center">
+          <li class="nav-item">
+            <a class="nav-link" href="./" target="_blank">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./serena" target="_blank">Serena</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./matthew" target="_blank">Matthew</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  <div class="instruction-header">
+    <h1>2024 Intro Project</h1>
+  </div>
+  <div class="container">
+    <h1>INSTRUCTIONS</h1>
+    <div class="row">
+      <div class="col-md-7">
+        <div class="instruction-body">
+          <ol class="list-group">
+            <li class="list-group-item">Name & Picture</li>
+            <li class="list-group-item">Navbar</li>
+            <li class="list-group-item">Button</li>
+            <li class="list-group-item">Bootstrap (rows & cols)</li>
+            <li class="list-group-item"><a href="https://vuejs.org/api/built-in-directives.html">Vue Directive</a></li>
+          </ol>
+        </div>
+      </div>
     </div>
-  </nav>
   </div>
 </template>
+
+<!-- Reqs:
+Name & Picture
+Navbar
+Button
+Bootstrap
+Vue directive (https://vuejs.org/api/built-in-directives.html)
+Ensure that you use Vue's Composition API
+
+Please name your page "{name}.vue" (without brackets)
+Please make sure you're on a branch with your name -->
 
 <style scoped>
 
@@ -60,6 +93,23 @@
 }
 
 body {
-    overflow-x: hidden !important;
+  overflow-x: hidden !important;
 }
+
+.instruction-header {
+  text-align: center;
+  padding-top: 1rem;
+}
+
+.instruction-body {
+  text-align: left;
+}
+
+.container {
+  background-color: #211642;
+  width: 100%;;
+  padding-left: 0%;
+  padding-right: 0%;
+}
+
 </style>
---
+++ pages/matthew.vue
@@ -0,0 +1,15 @@
+<template>
+  <navbar />
+
+  <div class="flex_container">
+    
+  </div>
+</template>
+
+<style scoped>
+
+.flex-container {
+  display: flex;
+}
+
+</style>
