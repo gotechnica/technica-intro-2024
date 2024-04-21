d57eba6058a71133c1b305db86de108413b28611
Serena <pizzalover2317@gmail.com>
2024-04-20 21:21:44 -0400

    main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,44 @@
 <template>
-  <navbar />
-  <div class="instruction-header">
-    <h1>2024 Intro Project</h1>
-  </div>
-  <div class="container">
-    <h1>INSTRUCTIONS</h1>
-    <div class="row">
-      <div class="col-md-7">
-        <div class="instruction-body">
-          <ol class="list-group">
-            <li class="list-group-item">Name & Picture</li>
-            <li class="list-group-item">Navbar</li>
-            <li class="list-group-item">Button</li>
-            <li class="list-group-item">Bootstrap (rows & cols)</li>
-            <li class="list-group-item"><a href="https://vuejs.org/api/built-in-directives.html">Vue Directive</a></li>
-          </ol>
+  <div class="wrapper">
+    <navbar />
+    <div class="flex-container">
+      <div class="instruction-header">
+        <h1>2024 Intro Project</h1>
+      </div>
+      <h1>INSTRUCTIONS</h1>
+      <div class="row">
+        <div class="col-md-7">
+          <p>For the intro project, we just wanna make sure everyone is comfortable using the tech stack. 
+            Everyone's goal is to make their own personal page telling the team a little bit about themselves.
+            The only restrictions we have are that you make it a vue page and you include:
+          </p>
+          <ul class = "bullets">
+            <li class="bullet">Name & picture</li>
+            <li class="bullet">Navbar</li>
+            <li class="bullet">Button</li>
+            <li class="bullet">Bootstrap (rows & cols)</li>
+            <li class="bullet">Vue Directive (v-if, v-show, etc. for more information, here's the <a href="https://vuejs.org/api/built-in-directives.html">Vue Directive Docs</a>)</li>
+          </ul>
+          <p>Good luck everyone! And if you need any questions, feel free to ask on Slack!</p>
+        </div>
+        <div class="col-md-5">
+          <img class = "yaya" src = "yaya.gif" />
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<!-- Reqs:
-Name & Picture
-Navbar
-Button
-Bootstrap
-Vue directive (https://vuejs.org/api/built-in-directives.html)
-Ensure that you use Vue's Composition API
-
-Please name your page "{name}.vue" (without brackets)
-Please make sure you're on a branch with your name -->
-
 <script>
 
 </script>
 
-<style scoped>
 
-body {
-  overflow-x: hidden !important;
+<style scoped>
+.wrapper {
+  background: #211642;
+  width: 100%;
+  min-height: 100vh;
 }
 
 .instruction-header {
@@ -47,15 +46,16 @@ body {
   padding-top: 1rem;
 }
 
-.instruction-body {
-  text-align: left;
+.flex-container {
+  margin-left: 3rem;
+  margin-right: 3rem;
+  color: white;
 }
 
-.container {
-  background-color: #211642;
-  width: 100%;;
-  padding-left: 0%;
-  padding-right: 0%;
+.yaya {
+  margin-left:2rem;
+  margin-top:0rem;
+  border-radius: 30px;
+  width: 90%;
 }
-
 </style>
