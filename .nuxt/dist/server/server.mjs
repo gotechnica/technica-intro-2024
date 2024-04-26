d03fd2fe1e9543fc633e48109262713e279c44c8
Valentina Hong <valentina.m.hong@gmail.com>
2024-04-25 20:56:18 -0400

    setting stuff up

--- .nuxt/dist/server/server.mjs
+++ .nuxt/dist/server/server.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///Users/serena/GitRepos/technica-2024/technica-intro-2024/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
+export { default } from "file:///Users/valentina/Technica/technica-intro-2024/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
