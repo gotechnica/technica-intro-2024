0c8fd45c225491de622ef7120b650bd80c197297
caramurphyy <murphylcara@gmail.com>
2024-04-25 20:18:23 -0400

    update styling and text

--- pages/cara.vue
+++ pages/cara.vue
@@ -27,35 +27,48 @@ export default {
 </script>
 
 <template>
-    <navbar />
+    <div class="wrapper">
+        <navbar />
 
-    <div class="container">
-        <div class="image-container">
-            <img src="public/cara_images/caraprofile.jpg" alt="Image" class="rounded-image" />
-        </div>
-        <div class="content">
-            <h1>cara!</h1>
-            <p>lorem ipsum</p>
-        </div>
-        <div class="centered-images">
+        <div class="container">
+            <div class="image-container">
+                <img src="public/cara_images/caraprofile.jpg" alt="Image" class="rounded-image" />
+            </div>
+            <div class="content">
+                <h1>cara!</h1>
+                <p>my name is cara murphy and I'm a sophmore computer science and immersive media design double
+                    major.<br>
+                    click the images below to learn more about me!
+                </p>
+            </div>
+            <div class="centered-images">
 
-            <div class="image-wrapper" v-on:click="updateOpacity1">
-                <img src="public/cara_images/caracafe.jpg" alt="Image 1" id="hover-image1" />
-                <div id="hover-text1">I like going to try new coffee shops every monday!</div>
-            </div>
-            <div class="image-wrapper" v-on:click="updateOpacity2">
-                <img src="public/cara_images/caratravel.jpg" alt="Image 2" id="hover-image2" />
-                <div id="hover-text2">I have been to </div>
-            </div>
-            <div class="image-wrapper" v-on:click="updateOpacity3">
-                <img src="public/cara_images/caraart.jpg" alt="Image 3" id="hover-image3" />
-                <div id="hover-text3">Text 3</div>
+                <div class="image-wrapper" v-on:click="updateOpacity1">
+                    <img src="public/cara_images/caracafe.jpg" alt="Image 1" id="hover-image1" />
+                    <div id="hover-text1">I like going to try new coffee shops every monday with eileen!</div>
+                </div>
+                <div class="image-wrapper" v-on:click="updateOpacity2">
+                    <img src="public/cara_images/caratravel.jpg" alt="Image 2" id="hover-image2" />
+                    <div id="hover-text2">I have been to 8 countries!</div>
+                </div>
+                <div class="image-wrapper" v-on:click="updateOpacity3">
+                    <img src="public/cara_images/caraart.jpg" alt="Image 3" id="hover-image3" />
+                    <div id="hover-text3">I'm an artist and I love to draw and paint. This is one of my artt100
+                        project's.
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style>
+.wrapper {
+    width: 100vw;
+    height: 100vh;
+    background: rgb(14, 25, 51);
+}
+
 .container {
     display: flex;
     flex-direction: column;
@@ -77,7 +90,7 @@ export default {
 
 .content {
     text-align: center;
-    margin-top: 20px;
+    margin-top: 15px;
 }
 
 .centered-images {
@@ -88,26 +101,46 @@ export default {
 
 .image-wrapper {
     position: relative;
-    margin: 0 10px;
+    margin: 5px 15px;
+
 }
 
+h1 {
+    color: white;
+}
+
+p {
+    color: white;
+}
 
 #hover-image1 {
     width: 250px;
     height: 250px;
     object-fit: cover;
+    border-radius: 1rem;
+    border-style: solid;
+    border-width: 0.2rem;
+    border-color: white;
 }
 
 #hover-image2 {
     width: 250px;
     height: 250px;
     object-fit: cover;
+    border-radius: 1rem;
+    border-style: solid;
+    border-width: 0.2rem;
+    border-color: white;
 }
 
 #hover-image3 {
     width: 250px;
     height: 250px;
     object-fit: cover;
+    border-radius: 1rem;
+    border-style: solid;
+    border-width: 0.2rem;
+    border-color: white;
 }
 
 #hover-text1 {
@@ -115,9 +148,9 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    background-color: "#0a224a";
+    background: rgb(14, 25, 51);
     color: white;
-    padding: 20px;
+    padding: 25px;
     font-size: 15px;
     opacity: 0;
     transition: opacity 0.3s;
@@ -128,9 +161,9 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    background-color: "#0a224a";
+    background: rgb(14, 25, 51);
     color: white;
-    padding: 20px;
+    padding: 25px;
     font-size: 15px;
     opacity: 0;
     transition: opacity 0.3s;
@@ -141,9 +174,9 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    background-color: "#0a224a";
+    background: rgb(14, 25, 51);
     color: white;
-    padding: 20px;
+    padding: 25px;
     font-size: 15px;
     opacity: 0;
     transition: opacity 0.3s;
